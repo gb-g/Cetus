3840f5c4a7d35dcfa34cab64d853ff1764345df7
CoolCNC <service@thecooltool.com>
2021-08-20 16:27:22 +0200

    Add Chinese translation

---
+++ translations/cetus_zh.ts
@@ -0,0 +1,377 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <source>About Cetus</source>
+        <translation>关于 Cetus 软件</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationMenuBar</name>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;文档</translation>
+    </message>
+    <message>
+        <source>&amp;Machine</source>
+        <translation>&amp;机器</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation>&amp;查看</translation>
+    </message>
+    <message>
+        <source>Show o&amp;ffsets</source>
+        <translation>显示偏置量</translation>
+    </message>
+    <message>
+        <source>Show v&amp;elocity</source>
+        <translation>显示速度</translation>
+    </message>
+    <message>
+        <source>Show &amp;distance to go</source>
+        <translation>显示走距</translation>
+    </message>
+    <message>
+        <source>Enable &amp;preview</source>
+        <translation>启用预览</translation>
+    </message>
+    <message>
+        <source>Sh&amp;ow machine limits</source>
+        <translation>显示机器位限</translation>
+    </message>
+    <message>
+        <source>S&amp;how program</source>
+        <translation>显示程序</translation>
+    </message>
+    <message>
+        <source>Show program e&amp;xtents</source>
+        <translation>显示程序范围</translation>
+    </message>
+    <message>
+        <source>Show program r&amp;apids</source>
+        <translation>显示程序快速移动</translation>
+    </message>
+    <message>
+        <source>Alpha-&amp;blend program</source>
+        <translation>阿尔法混合程序</translation>
+    </message>
+    <message>
+        <source>Sho&amp;w live plot</source>
+        <translation>显示实时刀路图</translation>
+    </message>
+    <message>
+        <source>Sho&amp;w tool</source>
+        <translation>显示刀具</translation>
+    </message>
+    <message>
+        <source>Show &amp;coordinate</source>
+        <translation>显示坐标</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <source>About &amp;Cetus</source>
+        <translation>关于 Cetus 软件</translation>
+    </message>
+    <message>
+        <source>&amp;Disconnect from Session</source>
+        <translation>从对话断开</translation>
+    </message>
+    <message>
+        <source>Sh&amp;utdown Session</source>
+        <translation>关机</translation>
+    </message>
+    <message>
+        <source>E&amp;xit User Interface</source>
+        <translation>退出用户界面</translation>
+    </message>
+    <message>
+        <source>Show &amp;spindle speed</source>
+        <translation>显示主轴速度</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationStatusBar</name>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AxisControls</name>
+    <message>
+        <source>Axis</source>
+        <translation>轴</translation>
+    </message>
+    <message>
+        <source>Jog Velocity</source>
+        <translation>微动速度</translation>
+    </message>
+</context>
+<context>
+    <name>Cetus</name>
+    <message>
+        <source>DRO</source>
+        <translation>相对零点偏置范围</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>预览显示</translation>
+    </message>
+    <message>
+        <source>Webcam</source>
+        <translation>网络摄像头</translation>
+    </message>
+</context>
+<context>
+    <name>CoolantControls</name>
+    <message>
+        <source>Coolant</source>
+        <translation>冷却剂</translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPanel</name>
+    <message>
+        <source>Configuration</source>
+        <translation>配置</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorTab</name>
+    <message>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+</context>
+<context>
+    <name>FeedrateControl</name>
+    <message>
+        <source>Feed Override</source>
+        <translation>进给倍率</translation>
+    </message>
+</context>
+<context>
+    <name>GantryConfigControl</name>
+    <message>
+        <source>X1 [plug X] offset:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> X2 [plug C] offset:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MachineStatusLabel</name>
+    <message>
+        <source>ESTOP</source>
+        <translation>紧急停止</translation>
+    </message>
+    <message>
+        <source>OFF</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <source>ON</source>
+        <translation>开启</translation>
+    </message>
+    <message>
+        <source>Not connected</source>
+        <translation>未连接</translation>
+    </message>
+</context>
+<context>
+    <name>ManualTab</name>
+    <message>
+        <source>Manual</source>
+        <translation>指南</translation>
+    </message>
+</context>
+<context>
+    <name>MaximumVelocityControl</name>
+    <message>
+        <source>Maximum Velocity</source>
+        <translation>最大速度</translation>
+    </message>
+</context>
+<context>
+    <name>MdiTab</name>
+    <message>
+        <source>MDI</source>
+        <translation>人工数据输入</translation>
+    </message>
+    <message>
+        <source>Active GCodes</source>
+        <translation>运转的G代码指令</translation>
+    </message>
+</context>
+<context>
+    <name>PositionOffsetLabel</name>
+    <message>
+        <source>Position: </source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <source>Relative Actual</source>
+        <translation>相对实际坐标</translation>
+    </message>
+    <message>
+        <source>Machine Actual</source>
+        <translation>机床实际坐标</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewPanel</name>
+    <message>
+        <source>Enable preview</source>
+        <translation>启用预览</translation>
+    </message>
+</context>
+<context>
+    <name>PrinterTab</name>
+    <message>
+        <source>Printer</source>
+        <translation>打印机</translation>
+    </message>
+    <message>
+        <source>E0 temperature - set to </source>
+        <translation>E0 喷头温度-设置为</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>开启</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <source>Bed temperature - set to </source>
+        <translation>床温设置为</translation>
+    </message>
+    <message>
+        <source>Fan speed</source>
+        <translation>风扇速度</translation>
+    </message>
+    <message>
+        <source>Remove Filament</source>
+        <translation>移除材料</translation>
+    </message>
+    <message>
+        <source>Insert Filament</source>
+        <translation>装入材料</translation>
+    </message>
+    <message>
+        <source>Calibrate</source>
+        <translation>校准</translation>
+    </message>
+    <message>
+        <source>Probe</source>
+        <translation>检查</translation>
+    </message>
+</context>
+<context>
+    <name>RapidrateControl</name>
+    <message>
+        <source>Rapid Override</source>
+        <translation>快速进给倍率</translation>
+    </message>
+</context>
+<context>
+    <name>SpindleControls</name>
+    <message>
+        <source>Spindle</source>
+        <translation>主轴</translation>
+    </message>
+</context>
+<context>
+    <name>SpindlerateControl</name>
+    <message>
+        <source>Spindle Override</source>
+        <translation>主轴倍率</translation>
+    </message>
+</context>
+<context>
+    <name>StyledDroBase</name>
+    <message>
+        <source>DRO</source>
+        <translation>相对零点偏置范围</translation>
+    </message>
+    <message>
+        <source>Position</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <source>Distance to Go</source>
+        <translation>走距</translation>
+    </message>
+    <message>
+        <source>Rad</source>
+        <translation>弧度</translation>
+    </message>
+    <message>
+        <source>Dia</source>
+        <translation>直径</translation>
+    </message>
+    <message>
+        <source>Extra</source>
+        <translation>额外的</translation>
+    </message>
+    <message>
+        <source>Vel</source>
+        <translation>速度</translation>
+    </message>
+    <message>
+        <source>DTG</source>
+        <translation>移动距离</translation>
+    </message>
+    <message>
+        <source>%1 Offsets</source>
+        <translation>%1 偏置</translation>
+    </message>
+    <message>
+        <source>G92 Offsets</source>
+        <translation>G92 偏置</translation>
+    </message>
+    <message>
+        <source>Tool Offsets</source>
+        <translation>刀具偏置</translation>
+    </message>
+    <message>
+        <source>S%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ToolStatusLabel</name>
+    <message>
+        <source>Tool %1, offset: %2, diameter: %3</source>
+        <translation>工具 %1, 偏置 %2, 直径 %3</translation>
+    </message>
+    <message>
+        <source>No Tool</source>
+        <translation>无刀具</translation>
+    </message>
+</context>
+<context>
+    <name>WebcamPanel</name>
+    <message>
+        <source>No webcam connected.</source>
+        <translation>无网络摄像头连接</translation>
+    </message>
+    <message>
+        <source>Turn on the switch.</source>
+        <translation>打开开关</translation>
+    </message>
+</context>
+</TS>
